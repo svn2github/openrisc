00ffbc1a038c3b2dc53ba14b3469363a07c3b8f9
jeremybennett <jeremybennett@04c7f051-eaf3-4997-922d-f29d9d8c0f3f>
2010-07-04 13:21:49 +0000

    Removed. uClibc now obsolete. All other contents moved to unified GNU source and patches trees.
    git-svn-id: http://opencores.org/ocsvn/openrisc/openrisc@166 04c7f051-eaf3-4997-922d-f29d9d8c0f3f

--- trunk/gcc/uClibc-0.9.29/libc/misc/internals/interp.c
+++
@@ -1,5 +0,0 @@
-/* Force shared libraries to know about the correct library loader */
-#include <features.h>
-#ifdef __HAVE_ELF__
-const char __dl_ldso__[] __attribute__ ((section  (".interp"))) ="/ld-uClibc.so.0";
-#endif
