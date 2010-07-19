8190b2a7cd5bf421974c24291eb0065c6ce8c07b
jeremybennett <jeremybennett@04c7f051-eaf3-4997-922d-f29d9d8c0f3f>
2010-07-19 17:43:24 +0000

    I port to the OpenRISC 1000 32-bit version.
    git-svn-id: http://opencores.org/ocsvn/openrisc/openrisc@207 04c7f051-eaf3-4997-922d-f29d9d8c0f3f

---
+++ trunk/gnu-src/newlib-1.18.0/libgloss/or32/uart-dummy.c
@@ -0,0 +1,45 @@
+/* uart-dummy.c. UART dummy initialization function.
+
+   Copyright (C) 2004, Jacob Bower
+   Copyright (C) 2010, Embecosm Limited <info@embecosm.com>
+ 
+   Contributor Jeremy Bennett <jeremy.bennett@embecosm.com>
+  
+   This file is part of Newlib.
+
+   The original work by Jacob Bower is provided as-is without any kind of
+   warranty. Use it at your own risk!
+
+   All subsequent work is bound by version 3 of the GPL as follows.
+ 
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the Free
+   Software Foundation; either version 3 of the License, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+   more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program.  If not, see <http:#www.gnu.org/licenses/>.             */
+/* -------------------------------------------------------------------------- */
+/* This program is commented throughout in a fashion suitable for processing
+   with Doxygen.                                                              */
+/* -------------------------------------------------------------------------- */
+
+
+/* -------------------------------------------------------------------------- */
+/*!Dummy UART initialization.
+
+   To allow the same crt0 to be used with or without a UART, we provide a
+   dummy UART initialization, which can be called when the UART is not
+   actually present.
+
+   It does nothing!
+/* -------------------------------------------------------------------------- */
+void
+_uart_init ()
+{
+}	/* _uart_init () */
